e1ffd69110e4197a1a8710662d017147b8eb9561
Audumbar Kalokhe <audumbar.kalokhe@AudumbarMacbook.local>
2023-08-10 14:42:35 +0530

    Customies animation

--- component/screens/CustomAnimation.tsx
+++ component/screens/CustomAnimation.tsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View } from "react-native";
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withSpring,
+  withTiming,
+} from "react-native-reanimated";
 
 const CustomAnimation = () => {
-  return <View style={{ flex: 1 }}></View>;
+  const intialAnimation = useSharedValue(200);
+  const initialAnimateStyle = useAnimatedStyle(() => ({
+    transform: [{ translateX: withSpring(intialAnimation.value) }],
+  }));
+  useEffect(() => {
+    intialAnimation.value = withRepeat(
+      withTiming(-intialAnimation.value, {
+        duration: 2000,
+        easing: Easing.steps(5),
+      }),
+      -1,
+      true,
+      () => {
+        console.log("Hi");
+      }
+    );
+  }, []);
+  return (
+    <View style={{ flex: 1, justifyContent: "center" }}>
+      <Animated.View
+        style={[
+          {
+            height: 80,
+            width: 80,
+            margin: 20,
+            borderWidth: 1,
+            borderColor: "#b58df1",
+            borderRadius: 20,
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "red",
+            alignSelf: "center",
+          },
+          initialAnimateStyle,
+        ]}
+      ></Animated.View>
+    </View>
+  );
 };
 export default CustomAnimation;
